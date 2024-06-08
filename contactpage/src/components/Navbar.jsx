5f2558d08bebd6976cb72744df1126edd044c5fa
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-06-08 09:57:25 +0530

    contact info update

--- contactpage/src/components/Navbar.jsx
+++ contactpage/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/Frame.png'
-
+import contImg from '../assets/Service 24_7-pana.svg'
+import Message from '../assets/Vector.png'
 
 function Nav(){
     return(
@@ -13,16 +14,44 @@ function Nav(){
                 <h1>CONTACT US</h1>
                 <p>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. </p>
             </div>
-        </div>
 
         <div className="contactInfo">
             <div className="form">
-              this is the form section
+              <div className="anotherOptions">
+                <button className="others">
+                    <img src={Message} alt="" />
+                    SUPPORT CHAT
+                </button>
+                <button className="others">
+                    <img src={Message} alt="" />
+                    CALL NOW
+                </button>
+              </div>
+              <div className="emailOption">
+                <button className="email">
+                    <img src={Message} alt="" />
+                    EMAIL FORM
+                </button>
+              </div>
+
+              <div className="formInfo">
+                <label>Name:</label>
+                <input type="text" />
+                <label>Email:</label>
+                <input type="text" />
+                <label>Message: </label>
+                <input type="text" />
+                <button className="submit">
+                    Submit
+                </button>
+              </div>
             </div>
             <div className="img">
-                <h1>this is the image section</h1>
+                <img src={contImg} alt="" />
             </div>
         </div>
+        </div>
+
         </>
     )
 };
