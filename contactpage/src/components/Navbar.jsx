1aa03dfa5f873f6d41a6b56a808de6b26caac79c
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-06-10 14:39:01 +0530

    complete contact us page using react js

--- contactpage/src/components/Navbar.jsx
+++ contactpage/src/components/Navbar.jsx
@@ -1,59 +1,63 @@
-import logo from '../assets/Frame.png'
-import contImg from '../assets/Service 24_7-pana.svg'
-import Message from '../assets/Vector.png'
+import logo from "../assets/Frame.png";
+import contImg from "../assets/Service 24_7-pana.svg";
+import Message from "../assets/Vector.png";
 
-function Nav(){
-    return(
-        <>
-        <div className="navbar">
-            <img src={logo} alt="" />
+function Nav() {
+  return (
+    <>
+      <div className="navbar">
+        <img src={logo} alt="" />
+      </div>
+
+      <div className="contact">
+        <div className="contacthead">
+          <h1>CONTACT US</h1>
+          <p>
+            LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
+            WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
+            REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
+            EMAIL, OR SOCIAL MEDIA.{" "}
+          </p>
         </div>
 
-        <div className="contact">
-            <div className="contacthead">
-                <h1>CONTACT US</h1>
-                <p>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. </p>
+        <div className="contactInfo">
+          <div className="form">
+            <div className="anotherOptions">
+              <button className="others">
+                <img src={Message} alt="" />
+                SUPPORT CHAT
+              </button>
+              <button className="others">
+                <img src={Message} alt="" />
+                CALL NOW
+              </button>
+            </div>
+            <div className="emailOption">
+              <button className="email">
+                <img src={Message} alt="" />
+                EMAIL FORM
+              </button>
             </div>
 
-        <div className="contactInfo">
-            <div className="form">
-              <div className="anotherOptions">
-                <button className="others">
-                    <img src={Message} alt="" />
-                    SUPPORT CHAT
-                </button>
-                <button className="others">
-                    <img src={Message} alt="" />
-                    CALL NOW
-                </button>
-              </div>
-              <div className="emailOption">
-                <button className="email">
-                    <img src={Message} alt="" />
-                    EMAIL FORM
-                </button>
-              </div>
-
-              <div className="formInfo">
+            <div className="formInfo">
+              <form action="submit" className="submit">
                 <label>Name:</label>
                 <input type="text" />
                 <label>Email:</label>
                 <input type="text" />
                 <label>Message: </label>
-                <input type="text" />
-                <button className="submit">
-                    Submit
-                </button>
-              </div>
+                <textarea type="text" />
+                <button className="submitbtn">Submit</button>
+              </form>
             </div>
-            <div className="img">
-                <img src={contImg} alt="" />
-            </div>
+          </div>
+          <div className="img">
+            <img src={contImg} alt="" />
+          </div>
         </div>
-        </div>
+      </div>
+    </>
+  );
+}
 
-        </>
-    )
-};
-
-export default Nav
+export default Nav;
