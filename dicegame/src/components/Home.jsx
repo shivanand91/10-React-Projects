4fe153fea5d17517c9608749c45eeb41e5b2445b
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-06-11 12:39:04 +0530

    update

--- dicegame/src/components/Home.jsx
+++ dicegame/src/components/Home.jsx
@@ -1,12 +1,14 @@
+import Homedice from '../assets/dicesibg.png'
+
 function Home(){
     return(
         <>
         <div className="home-container">
             <div className="dice-img">
-
+               <img src={Homedice} alt="" />
             </div>
             <div className="dice-details">
-                
+
             </div>
         </div>
         </>
