11d019f6cdd23b0c85b184b8bdfedfe4a3b00693
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-06-11 12:36:38 +0530

    this is the require thing which is used for making dice game

---
+++ dicegame/src/components/Home.jsx
@@ -0,0 +1,16 @@
+function Home(){
+    return(
+        <>
+        <div className="home-container">
+            <div className="dice-img">
+
+            </div>
+            <div className="dice-details">
+                
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Home
